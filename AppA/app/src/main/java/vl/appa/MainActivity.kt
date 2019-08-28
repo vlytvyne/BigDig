e8763c02c8ce0dfe1b4602affbbd82e53d189760
Vadym Lytvynets <v.lytvynets61@gmail.com>
2019-08-28 13:20:23 +0300

    AppA markup

---
+++ AppA/app/src/main/java/vl/appa/MainActivity.kt
@@ -0,0 +1,40 @@
+package vl.appa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+        viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+
+		tabLayout.setupWithViewPager(viewPager)
+	}
+
+}
+
+private class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int) =
+        when (position) {
+            0 -> TestFragment.newInstance()
+            1 -> HistoryFragment.newInstance()
+            else -> throw Exception("Out of items")
+        }
+
+    override fun getPageTitle(position: Int) =
+        when (position) {
+            0 -> "Test"
+            1 -> "History"
+            else -> throw Exception("Out of items")
+        }
+
+
+    override fun getCount() = 2
+}
---
+++ AppA/app/src/main/java/vl/appa/TestFragment.kt
@@ -0,0 +1,21 @@
+package vl.appa
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+class TestFragment : Fragment() {
+
+	override fun onCreateView(inflater: LayoutInflater,
+							  container: ViewGroup?,
+							  savedInstanceState: Bundle?) =
+		inflater.inflate(R.layout.fragment_test, container, false)
+
+
+	companion object {
+
+		fun newInstance() = TestFragment()
+	}
+}
